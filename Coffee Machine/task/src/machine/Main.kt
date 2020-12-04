925648739159ee66643890b9e8cf88dcb21b8319
olegivo <olegivo@gmail.com>
2020-12-04 21:25:02 +0300

    Stage 2/6: Machines have needs

--- Coffee Machine/task/src/machine/Main.kt
+++ Coffee Machine/task/src/machine/Main.kt
@@ -1,11 +1,10 @@
 package machine
 
 fun main() {
-    println("""Starting to make a coffee
-Grinding coffee beans
-Boiling water
-Mixing boiled water with crushed coffee beans
-Pouring coffee into the cup
-Pouring some milk into the cup
-Coffee is ready!""")
+    println("Write how many cups of coffee you will need:")
+    val cupsCount = readLine()!!.toInt()
+    println("For $cupsCount cups of coffee you will need:")
+    println("${cupsCount * 200} ml of water")
+    println("${cupsCount * 50} ml of milk")
+    println("${cupsCount * 15} g of coffee beans")
 }
